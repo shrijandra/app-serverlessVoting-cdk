528210ee9564164320c87ae2f7f7818701bc185a
Donnie Prakoso <donnie.prakoso@gmail.com>
2021-10-03 14:43:19 +0800

    Adding CDK source code

---
+++ cdk/app.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+import os
+from aws_cdk import aws_iam as _iam
+from aws_cdk import aws_lambda as _lambda
+from aws_cdk import aws_dynamodb as _ddb
+from aws_cdk import aws_apigateway as _ag
+from aws_cdk import core
+from aws_cdk import aws_ec2 as _ec2
+from aws_cdk import aws_ecs as _ecs
+from aws_cdk import aws_ecs_patterns as _ecs_patterns
+from aws_cdk.aws_ecr import Repository
+from aws_cdk.pipelines import CodeBuildOptions, CodeBuildStep, CodePipeline, CodePipelineSource, ShellStep
+
+
+class VotingApiStack(core.Stack):
+    def __init__(self, scope: core.Construct, construct_id: str,
+                 **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # Model all required resources
+        ddb_table = _ddb.Table(
+            self,
+            id='{}-db'.format(construct_id),
+            table_name='{}-db'.format(construct_id),
+            partition_key=_ddb.Attribute(name='VOTE_ID',
+                                         type=_ddb.AttributeType.STRING),
+            removal_policy=core.RemovalPolicy.DESTROY,# THIS IS NOT RECOMMENDED FOR PRODUCTION USE
+            read_capacity=5,
+            write_capacity=5)
+
+        # IAM Roles
+        lambda_role = _iam.Role(
+            self,
+            id='{}-lambda-role'.format(construct_id),
+            assumed_by=_iam.ServicePrincipal('lambda.amazonaws.com'))
+
+        cw_policy_statement = _iam.PolicyStatement(effect=_iam.Effect.ALLOW)
+        cw_policy_statement.add_actions("logs:CreateLogGroup")
+        cw_policy_statement.add_actions("logs:CreateLogStream")
+        cw_policy_statement.add_actions("logs:PutLogEvents")
+        cw_policy_statement.add_actions("logs:DescribeLogStreams")
+        cw_policy_statement.add_resources("*")
+        lambda_role.add_to_policy(cw_policy_statement)
+
+        # Add role for DynamoDB
+        dynamodb_policy_statement = _iam.PolicyStatement(
+            effect=_iam.Effect.ALLOW)
+        dynamodb_policy_statement.add_actions("dynamodb:PutItem")
+        dynamodb_policy_statement.add_actions("dynamodb:GetItem")
+        dynamodb_policy_statement.add_actions("dynamodb:UpdateItem")
+        dynamodb_policy_statement.add_actions("dynamodb:DeleteItem")
+        dynamodb_policy_statement.add_actions("dynamodb:Scan")
+        dynamodb_policy_statement.add_actions("dynamodb:Query")
+        dynamodb_policy_statement.add_actions("dynamodb:ConditionCheckItem")
+        dynamodb_policy_statement.add_resources(ddb_table.table_arn)
+        lambda_role.add_to_policy(dynamodb_policy_statement)
+
+        # AWS Lambda Functions
+        fnLambda_vote = _lambda.Function(
+            self,
+            "{}-function-vote".format(construct_id),
+            code=_lambda.AssetCode("../voting-api/vote_option"),
+            handler="app.handler",
+            timeout=core.Duration.seconds(60),
+            role=lambda_role,
+            runtime=_lambda.Runtime.PYTHON_3_8)
+        fnLambda_vote.add_environment("TABLE_NAME", ddb_table.table_name)
+
+        fnLambda_options= _lambda.Function(
+            self,
+            "{}-function-options".format(construct_id),
+            code=_lambda.AssetCode("../voting-api/get_options"),
+            handler="app.handler",
+            timeout=core.Duration.seconds(60),
+            role=lambda_role,
+            runtime=_lambda.Runtime.PYTHON_3_8)
+        fnLambda_options.add_environment("TABLE_NAME", ddb_table.table_name)
+
+        api = _ag.RestApi(
+            self,
+            id="{}-api-gateway".format(construct_id),
+        )
+
+        int_vote = _ag.LambdaIntegration(fnLambda_vote)
+        int_options = _ag.LambdaIntegration(fnLambda_options)
+
+        res_data = api.root.add_resource('vote')
+        res_data.add_method('POST',int_vote)
+        res_data.add_method('GET',int_options)
+
+        core.CfnOutput(self, "{}-output-dynamodbTable".format(construct_id),
+                       value=ddb_table.table_name, export_name="{}-ddbTable".format(construct_id))
+        core.CfnOutput(self, "{}-output-apiEndpointURL".format(construct_id),
+                       value=api.url, export_name="{}-apiEndpointURL".format(construct_id))
+
+AWS_ACCOUNT_ID = os.getenv("CDK_DEFAULT_ACCOUNT")
+AWS_REGION = os.getenv("CDK_DEFAULT_REGION")
+STACK_NAME = 'votingapp'
+app = core.App()
+
+# Defining staging environment
+ENV_NAME = "{}-staging".format(STACK_NAME)
+votingapi_staging_stack = VotingApiStack(app, "{}-api".format(ENV_NAME))
+core.Tags.of(votingapi_staging_stack).add('Name', STACK_NAME)
+
+# Defining staging environment
+ENV_NAME = "{}-prod".format(STACK_NAME)
+votingapi_prod_stack = VotingApiStack(app, "{}-api".format(ENV_NAME))
+core.Tags.of(votingapi_prod_stack).add('Name', STACK_NAME)
+
+app.synth()
